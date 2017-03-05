2d013640baf2d02d7c12a4d26865e4829a339a07
sinpai <monaco90@gmail.com>
2017-03-05 13:26:00 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+ruby '2.3.3'
+
+gem 'ffi'
+gem 'rake-compiler'
+gem 'benchmark-ips'
+gem 'array_stats', git: 'https://github.com/bkoski/array_stats.git'
---
+++ ext/percentile-go/percentile.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+func Percentile(input []float64, percent float64) (percentile float64) {
+	if len(input) == 0 || percent == 0 {
+		return math.NaN()
+	}
+	c := sortedCopy(input)
+	index := (percent / 100) * float64(len(c))
+	if index == float64(int64(index)) {
+		i := float64ToInt(index)
+		percentile = mean([]float64{c[i-1], c[i]})
+	} else if index >= 1 {
+		i := float64ToInt(index)
+		percentile = c[i-1]
+	} else {
+		return math.NaN()
+	}
+	return percentile
+}
+func float64ToInt(input float64) (output int) {
+	r := round(input)
+	return int(r)
+}
+func copyslice(input []float64) []float64 {
+	s := make([]float64, len(input))
+	copy(s, input)
+	return s
+}
+func sortedCopy(input []float64) (copy []float64) {
+	if sort.Float64sAreSorted(input) {
+		return input
+	}
+	copy = copyslice(input)
+	sort.Float64s(copy)
+	return
+}
+func mean(input []float64) float64 {
+	sum := sum(input)
+	return sum / float64(len(input))
+}
+func sum(input []float64) (sum float64) {
+	for _, n := range input {
+		sum += n
+	}
+	return sum
+}
+func round(input float64) (rounded float64) {
+	_, decimal := math.Modf(input)
+
+	if decimal >= 0.5 {
+		rounded = math.Ceil(input)
+	} else {
+		rounded = math.Floor(input)
+	}
+	return rounded
+}
+func main() {}
